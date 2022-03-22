5111278a0803f167113493ea19ebde728b69fdf7
nikita <sak-191_947433@volsu.ru>
2022-03-22 18:14:13 +0300

    Поле ввода Обновлено, добавлены названия. закончено

--- app/src/main/java/com/example/dungeonsanddragons/Characteristics.kt
+++ app/src/main/java/com/example/dungeonsanddragons/Characteristics.kt
@@ -1,5 +1,7 @@
 package com.example.dungeonsanddragons
 
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
@@ -8,6 +10,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import com.example.dungeonsanddragons.databinding.FragmentCharacteristicsBinding
 
 
 class Characteristics : Fragment() {
@@ -15,12 +18,18 @@ class Characteristics : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        var binding = FragmentCharacteristicsBinding.inflate(inflater, container, false)
+        binding.fieldName.name.text = getString(R.string.name)
+        binding.fieldLevel.name.text = getString(R.string.level)
+        binding.fieldRace.name.text = getString(R.string.race)
+        binding.fieldXp.name.text = getString(R.string.xp)
+
         val field_name = view?.findViewById<View>(R.id.linearLayout)
         if (field_name != null) {
             field_name.setBackgroundResource(R.drawable.shape_for_field)
         }
-        return inflater.inflate(R.layout.fragment_characteristics, container, false)
+        return binding.root
     }
 
 
